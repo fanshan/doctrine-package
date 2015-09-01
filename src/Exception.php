2597f4a6db8c6853f285194a5075b4f78c0d4ba9
Gauthier Delamarre <gdelamarre@gmail.com>
2015-09-01 18:33:39 +0200

    Add package specific Exception

---
+++ src/Exception.php
@@ -0,0 +1,15 @@
+<?php
+    /**
+     * Created by PhpStorm.
+     * User: gauthier
+     * Date: 01/09/15
+     * Time: 18:16
+     */
+    
+    namespace ObjectivePHP\DoctrinePackage;
+    
+    
+    class Exception extends \Exception
+    {
+        
+    }
